45460bcf89caee5b355072a6f0d7088a9c90754a
Mattias Kjellsson <u.m.kjellsson@gmail.com>
2021-08-18 13:04:16 +0800

    restructure code again... I think it's clearer... (?)

--- lib/main2.dart
+++ lib/main2.dart
@@ -5,6 +5,110 @@ import 'package:fixnum/fixnum.dart';
 
 late final SessionBuilder _aliceSessionBuilder;
 late final InMemorySignalProtocolStore _aliceStore;
+late final SessionCipher _bobSessionCipher;
+
+Future<void> main() async {
+  final aliAddress = SignalProtocolAddress('ali', 1);
+  final bobAddress = SignalProtocolAddress('bob', 1);
+
+  //////////////////////////////////////////////////////////////////////////////
+
+  createAliceStoreAndBuilder(bobAddress);
+
+  //////////////////////////////////////////////////////////////////////////////
+  //Alice receive kind of this stuff.
+  final bobPreKey = await createBobStore(aliAddress);
+
+  //Start Alice session
+  await _aliceSessionBuilder.processPreKeyBundle(bobPreKey);
+  final aliceSessionCipher = SessionCipher.fromStore(_aliceStore, bobAddress);
+
+  // Alice send a message
+  final toServer =
+      await encryptMessage(aliceSessionCipher, 'Message from alice');
+
+  //////////////////////////////////////////////////////////////////////////////
+
+  //Bob decrypt first message from Alice. _This is special!_
+  final fromServer = toServer;
+
+  final f = await _bobSessionCipher.decrypt(PreKeySignalMessage(fromServer));
+  print(utf8.decode(f));
+
+  //////////////////////////////////////////////////////////////////////////////
+
+  //Bob send a message
+  final bobOutgoingMessage =
+      await encryptMessage(_bobSessionCipher, 'Message from bob');
+
+  // Alice receive message
+  final alicePlaintext =
+      await decryptMessage(aliceSessionCipher, bobOutgoingMessage);
+
+  print(alicePlaintext);
+
+  //////////////////////////////////////////////////////////////////////////////
+
+  //Alice send message
+  final aliceOutgoing2 =
+      await encryptMessage(aliceSessionCipher, 'Second message from alice');
+
+  // Bob receive message
+  final alice2Plaintext =
+      await decryptMessage(_bobSessionCipher, aliceOutgoing2);
+  print(alice2Plaintext);
+
+  //////////////////////////////////////////////////////////////////////////////
+
+  // Bob send another message
+  final bobOutgoing2 =
+      await encryptMessage(_bobSessionCipher, 'Second message from bob');
+
+  // Alice receive another message.
+  final bob2Plaintext = await decryptMessage(aliceSessionCipher, bobOutgoing2);
+  print(bob2Plaintext);
+
+  //////////////////////////////////////////////////////////////////////////////
+
+  //Alice send message
+  final aliceOutgoing3 =
+      await encryptMessage(aliceSessionCipher, 'Third message from alice');
+
+  // Bob receive message
+  final alice3Plaintext =
+      await decryptMessage(_bobSessionCipher, aliceOutgoing3);
+  print(alice3Plaintext);
+
+  //////////////////////////////////////////////////////////////////////////////
+
+  // Bob send another message
+  final bobOutgoing3 =
+      await encryptMessage(_bobSessionCipher, 'Third message from bob');
+
+  // Alice receive another message.
+  final bob3Plaintext = await decryptMessage(aliceSessionCipher, bobOutgoing3);
+  print(bob3Plaintext);
+
+  //////////////////////////////////////////////////////////////////////////////
+
+  // Bob send another message
+  final bobOutgoing4 =
+      await encryptMessage(_bobSessionCipher, 'Fourth message from bob');
+
+  // Alice receive another message.
+  final bob4Plaintext = await decryptMessage(aliceSessionCipher, bobOutgoing4);
+  print(bob4Plaintext);
+
+  //////////////////////////////////////////////////////////////////////////////
+
+  // Bob send another message
+  final bobOutgoing5 =
+      await encryptMessage(_bobSessionCipher, 'Fifth message from bob');
+
+  // Alice receive another message.
+  final bob5Plaintext = await decryptMessage(aliceSessionCipher, bobOutgoing5);
+  print(bob5Plaintext);
+}
 
 void createAliceStoreAndBuilder(SignalProtocolAddress receiverAddress) {
   final aliGeneratedKey = Curve.generateKeyPair();
@@ -17,43 +121,42 @@ void createAliceStoreAndBuilder(SignalProtocolAddress receiverAddress) {
       SessionBuilder.fromSignalStore(_aliceStore, receiverAddress);
 }
 
-late final InMemorySignalProtocolStore _bobStore;
-
-Future<PreKeyBundle> createBobStore() async {
+Future<PreKeyBundle> createBobStore(SignalProtocolAddress aliAddress) async {
   final ECKeyPair bobGeneratedKey = Curve.generateKeyPair();
   final ECKeyPair bobPreKeyPair = Curve.generateKeyPair();
   final ECKeyPair bobSignedPreKeyPair = Curve.generateKeyPair();
   final int bobDeviceId = 1;
   final int bobPreKeyId = 31337;
   final int signedPreKeyId = 22;
+  final InMemorySignalProtocolStore bobStore;
 
-  _bobStore = InMemorySignalProtocolStore(
+  bobStore = InMemorySignalProtocolStore(
       IdentityKeyPair(
           IdentityKey(bobGeneratedKey.publicKey), bobGeneratedKey.privateKey),
       generateRegistrationId(false));
 
   final Uint8List bobSignedPreKeySignature = Curve.calculateSignature(
-      await _bobStore
+      await bobStore
           .getIdentityKeyPair()
           .then((value) => value.getPrivateKey()),
       bobSignedPreKeyPair.publicKey.serialize());
 
   final bobPreKey = PreKeyBundle(
-    await _bobStore.getLocalRegistrationId(),
+    await bobStore.getLocalRegistrationId(),
     bobDeviceId,
     bobPreKeyId,
     bobPreKeyPair.publicKey,
     signedPreKeyId,
     bobSignedPreKeyPair.publicKey,
     bobSignedPreKeySignature,
-    await _bobStore.getIdentityKeyPair().then((value) => value.getPublicKey()),
+    await bobStore.getIdentityKeyPair().then((value) => value.getPublicKey()),
   );
 
   // Set keys in bob's store.
-  await _bobStore.storePreKey(
+  await bobStore.storePreKey(
       bobPreKeyId, PreKeyRecord(bobPreKey.getPreKeyId(), bobPreKeyPair));
 
-  await _bobStore.storeSignedPreKey(
+  await bobStore.storeSignedPreKey(
       signedPreKeyId,
       SignedPreKeyRecord(
           signedPreKeyId,
@@ -61,92 +164,10 @@ Future<PreKeyBundle> createBobStore() async {
           bobSignedPreKeyPair,
           bobSignedPreKeySignature));
 
-  return Future.value(bobPreKey);
-}
-
-Future<void> main() async {
-  final aliAddress = SignalProtocolAddress('ali', 1);
-  final bobAddress = SignalProtocolAddress('bob', 1);
-
-  //////////////////////////////////////////////////////////////////////////////
-
-  createAliceStoreAndBuilder(bobAddress);
-
-  //////////////////////////////////////////////////////////////////////////////
-  //Alice receive kind of this stuff.
-  final bobPreKey = await createBobStore();
-
-  //Start Alice session
-  await _aliceSessionBuilder.processPreKeyBundle(bobPreKey);
-  final aliceSessionCipher = SessionCipher.fromStore(_aliceStore, bobAddress);
-
-  // Alice send a message
-  final toServer =
-      await encryptMessage(aliceSessionCipher, 'Message from alice');
-
-  //////////////////////////////////////////////////////////////////////////////
   // Init bob's session cipher
+  _bobSessionCipher = SessionCipher.fromStore(bobStore, aliAddress);
 
-  final bobSessionCipher = SessionCipher.fromStore(_bobStore, aliAddress);
-
-  //////////////////////////////////////////////////////////////////////////////
-
-  //Bob decrypt first message from Alice. _This is special!_
-  final fromServer = toServer;
-
-  final f = await bobSessionCipher.decrypt(PreKeySignalMessage(fromServer));
-  print(utf8.decode(f));
-
-  //////////////////////////////////////////////////////////////////////////////
-  //Bob send a message
-  final bobOutgoingMessage =
-      await encryptMessage(bobSessionCipher, 'Message from bob');
-
-  // Alice receive message
-  final alicePlaintext =
-      await decryptMessage(aliceSessionCipher, bobOutgoingMessage);
-
-  print(alicePlaintext);
-
-  //////////////////////////////////////////////////////////////////////////////
-  //Alice send message
-  final aliceOutgoing2 =
-      await encryptMessage(aliceSessionCipher, 'Second message from alice');
-
-  // Bob receive message
-  final alice2Plaintext =
-      await decryptMessage(bobSessionCipher, aliceOutgoing2);
-  print(alice2Plaintext);
-
-  //////////////////////////////////////////////////////////////////////////////
-
-  // Bob send another message
-  final bobOutgoing2 =
-      await encryptMessage(bobSessionCipher, 'Second message from bob');
-
-  // Alice receive another message.
-  final bob2Plaintext = await decryptMessage(aliceSessionCipher, bobOutgoing2);
-  print(bob2Plaintext);
-
-  //////////////////////////////////////////////////////////////////////////////
-  //Alice send message
-  final aliceOutgoing3 =
-      await encryptMessage(aliceSessionCipher, 'Third message from alice');
-
-  // Bob receive message
-  final alice3Plaintext =
-      await decryptMessage(bobSessionCipher, aliceOutgoing3);
-  print(alice3Plaintext);
-
-  //////////////////////////////////////////////////////////////////////////////
-
-  // Bob send another message
-  final bobOutgoing3 =
-      await encryptMessage(bobSessionCipher, 'Third message from bob');
-
-  // Alice receive another message.
-  final bob3Plaintext = await decryptMessage(aliceSessionCipher, bobOutgoing3);
-  print(bob3Plaintext);
+  return Future.value(bobPreKey);
 }
 
 Future<Uint8List> encryptMessage(SessionCipher cipher, String clearText) async {
