ff9430d013b00d43bf3529b90b1bdccc18ba2f4a
Mattias Kjellsson <u.m.kjellsson@gmail.com>
2021-08-19 09:46:46 +0800

    Add this little thing... :man_shrugging:

---
+++ lib/server_communication.dart
@@ -0,0 +1,41 @@
+import 'package:libsignal_protocol_dart/src/state/pre_key_bundle.dart';
+import 'package:libsignal_protocol_dart/src/signal_protocol_address.dart';
+import 'package:libsignal_protocol_dart/src/session_cipher.dart';
+
+import 'dart:typed_data';
+
+import 'communication.dart';
+import 'key_api.dart';
+
+class ServerConnection implements Communication {
+  late final KeyApi _keyApi = KeyApi();
+  late final SessionCipher sessionCipher;
+
+  @override
+  Future<void> createAliceStoreAndBuilder(
+      {required SignalProtocolAddress receiverAddress,
+      required PreKeyBundle preKey}) {
+    // TODO: implement createAliceStoreAndBuilder
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<String> decryptMessage(
+      {required SessionCipher cipher, required Uint8List fromServer}) {
+    // TODO: implement decryptMessage
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Uint8List> encryptMessage(
+      {required SessionCipher cipher, required String clearText}) {
+    // TODO: implement encryptMessage
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> start({required String alice, required String bob}) {
+    // TODO: implement start
+    throw UnimplementedError();
+  }
+}
